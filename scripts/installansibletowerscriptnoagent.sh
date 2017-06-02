b8710bd6918cf1dda08565a48b1c26b4ae41eaf3
PraveenAnil <praveendosth@gmail.com>
2017-06-02 21:50:59 +0530

    Update installansibletowerscriptnoagent.sh

--- scripts/installansibletowerscriptnoagent.sh
+++ scripts/installansibletowerscriptnoagent.sh
@@ -21,10 +21,10 @@ pip install setuptools --upgrade
 pip install azure==2.0.0rc6 --upgrade
 
 #Disable SSH Copy prompt#
-echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
-#echo "PasswordAuthentication yes" >> /etc/ssh/ssh_config
-
-#echo 'demouser:demoPassword1!' | chpasswd
+echo "StrictHostKeyChecking no" >> /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication\ no/PasswordAuthentication\ yes/g' /etc/ssh/sshd_config
+sed -i 's/ChallengeResponseAuthentication\ no/ChallengeResponseAuthentication\ yes/g' /etc/ssh/sshd_config
+echo 'demouser:demoPassword1!' | chpasswd
 
 ## The following code generates SSH Keys and copies it to other hosts ##  
 #ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -b 4096 -N ''
